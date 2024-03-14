d23e3fbfb52d88b254f85d1691846e0bfa79c317
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2024-03-14 17:18:15 +0000

    NB work

---
+++ clean_outputs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Cleaning notebook outputs"
+
+rm -rv _sicfile/
+rm -rv data/*
+rm -rv logs/*
+rm -rv network_datasets/*
+rm -rv notebook/*
+rm -rv processed/*
+rm -rv results/*
+rm -v *.{csv,json,log}
