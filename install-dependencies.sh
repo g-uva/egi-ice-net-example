813cf435b9e64d0192541228768c65619e0e560c
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-05-19 14:04:48 +0200

    Removing ipykernel setting.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y libhdf5-dev libnetcdf-dev
 
 pip install -r /home/jovyan/requirements.txt
 
-python -m ipykernel install --user --name=icenet --display-name "Python (IceNet)"
+# python -m ipykernel install --user --name=icenet --display-name "Python (IceNet)"
