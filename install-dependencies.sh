17ceec828ff042855a06e1be0332d111b7047867
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-06-02 09:44:22 +0200

    Minor fix.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 sudo apt-get install -y libhdf5-dev libnetcdf-dev
 
-pip install -r /home/jovyan/egi-ice-net-example/requirements.txt
+pip install -r /home/jovyan/icenet-notebook/requirements.txt
